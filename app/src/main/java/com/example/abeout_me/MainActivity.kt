5985d3ea11f56204323c7511b1151a50dd58dbfe
haftomgebremeskel <hafmob.inc@gmail.com>
2022-09-16 00:11:54 -0600

    Initial commit

---
+++ app/src/main/java/com/example/abeout_me/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.abeout_me
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.example.abeout_me.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("HAFTOM GEBREMESKEL")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View){
+
+        binding.apply {
+            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+    }
+}
---
+++ app/src/main/java/com/example/abeout_me/MyName.kt
@@ -0,0 +1,3 @@
+package com.example.abeout_me
+
+data class MyName(var name: String = "", var nickname: String = "")
